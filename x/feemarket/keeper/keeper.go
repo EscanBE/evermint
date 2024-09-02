4fb10bf39ef6227725f4004277d4980c58a63a7f
VictorTrustyDev <me@victortrusty.dev>
2024-09-03 01:18:34 +0700

    cleanup code

--- x/feemarket/keeper/keeper.go
+++ x/feemarket/keeper/keeper.go
@@ -1,8 +1,6 @@
 package keeper
 
 import (
-	"math/big"
-
 	"github.com/cometbft/cometbft/libs/log"
 	"github.com/cosmos/cosmos-sdk/codec"
 	storetypes "github.com/cosmos/cosmos-sdk/store/types"
@@ -11,10 +9,6 @@ import (
 
 	"github.com/EscanBE/evermint/v12/x/feemarket/types"
 )
-
-// KeyPrefixBaseFeeV1 TODO: Temporary will be removed with params refactor PR
-// TODO EB: remove this
-var KeyPrefixBaseFeeV1 = []byte{2}
 
 // Keeper grants access to the Fee Market module state.
 type Keeper struct {
@@ -58,17 +52,4 @@ func (k Keeper) WithEvmKeeper(evmKeeper types.EvmKeeper) Keeper {
 // Logger returns a module-specific logger.
 func (k Keeper) Logger(ctx sdk.Context) log.Logger {
 	return ctx.Logger().With("module", types.ModuleName)
-}
-
-// GetBaseFeeV1 get the base fee from v1 version of states.
-// return nil if base fee is not enabled
-// TODO: Figure out if this will be deleted ?
-// TODO EB: remove this
-func (k Keeper) GetBaseFeeV1(ctx sdk.Context) *big.Int {
-	store := ctx.KVStore(k.storeKey)
-	bz := store.Get(KeyPrefixBaseFeeV1)
-	if len(bz) == 0 {
-		return nil
-	}
-	return new(big.Int).SetBytes(bz)
 }
